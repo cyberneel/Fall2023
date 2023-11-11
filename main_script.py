88191222b8216af4cc3343327929c84dc8305430
cyberneel <neelesh.chevuri25@gmail.com>
2023-11-11 17:28:18 -0600

    removed canvasapi package

--- main_script.py
+++ main_script.py
@@ -3,17 +3,11 @@ from PIL import Image
 import streamlit as st
 import altair as alt
 from streamlit_calendar import calendar
-from canvasapi import Canvas
 import requests
 
 API_URL = "https://unt.instructure.com/api/v1/"
 API_KEY = "9082~1MuaTSggWfo8LFKsjyVFYGdbBV8KvK4RbfTGoiBtU8oEdVpNoPD2ipX2Fp3i4fKf"
 API_KEYM = "9082~PoqZCFiKGh0YegeAT4EBxzgUbdwuQcn8SIE1EAOTC07btruXEpbWQCNAmY8pdaz0"
-
-#apiTest = Canvas(API_URL, API_KEY)
-
-#course = apiTest.get_course(90487)
-#st.write(course.name)
 
 st.set_page_config(
     page_title="OnTime",
