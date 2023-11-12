74d5d4433f7b7e8b7818f36ec92fe83c385abbc6
cyberneel <neelesh.chevuri25@gmail.com>
2023-11-12 08:48:31 -0600

    made columns

--- pages/classes.py
+++ pages/classes.py
@@ -6,6 +6,8 @@ import altair as alt
 from streamlit_calendar import calendar
 import requests
 import datetime
+from io import BytesIO
+from PIL import Image
 
 
 API_URL = "https://unt.instructure.com"
@@ -34,27 +36,44 @@ def classes_page():
 
     userJson = requests.get("https://"+API_URL+API_EXT + "users/self?access_token="+ACCESS_TOKEN).json()
     st.title("Your Classes:")
-    response = requests.get("https://"+API_URL+API_EXT+"courses?access_token="+ACCESS_TOKEN)
+    dat = {'include[]': 'course_image'}
+    response = requests.get("https://"+API_URL+API_EXT+"courses?access_token="+ACCESS_TOKEN, data=dat)
     #st.write(response.json()[0]["name"])
     if not response.ok:
         st.write("GO TO SETTINGS AND ENTER INFO!")
         st.stop()
+    #st.write(response.json())
+    col1, col2, col3 = st.columns(3)
+    cols = [col1, col2, col3]
+    counting = 0
     for elem in response.json():
-        with st.expander(elem["name"]):
-            if("original_name" in elem):
-                st.write("Original Name: " + elem["original_name"])
+        if False:
+            img = elem["image_download_url"]
+            st.write(img)
+            #r = requests.get(img)
+            imggg = Image.open(img)
+            st.image(imggg, output_format="auto")
 
-            else:
-                st.write("Original Name: " + elem["name"])
+            #st.image(BytesIO(r.content))
+        with cols[counting]:
+            with st.expander(elem["name"]):
+                if("original_name" in elem):
+                    st.write("Original Name: " + elem["original_name"])
 
-            st.write("Course Code: " + elem["course_code"])
+                else:
+                    st.write("Original Name: " + elem["name"])
 
-            #calEvents = requests.get("https://"+API_URL+API_EXT+"calendar_events?type=assignment&access_token="+ACCESS_TOKEN+"&context_codes%5B%5D=course_" + str(elem["id"]))
-            #calEvents = requests.get("https://"+API_URL+API_EXT+"calendar_events?type=assignment" + "&context_codes%5B%5D=course_" + str(elem["id"]), {"Authorization": f"bearer {ACCESS_TOKEN}"})
-            #st.write(calEvents.json())
-            #st.write("Assignments Due TODAY:" + today)
-            #assignments = requests.get("https://"+API_URL+API_EXT+"calendar_events?access_token=" + ACCESS_TOKEN + "&type=assignment&context_codes%5B%5D=user_" + str(userJson["id"]) + "&context_codes%5B%5D=course_" + str(elem["id"]) + "&start_date=" + yesterday + "&end_date=" + today + "&per_page=50").json()
-            #for ass in assignments:
-            #    st.write(ass["title"])
+                st.write("Course Code: " + elem["course_code"])
+                counting = counting + 1
+                counting = counting % 3
+                #st.image(img)
+
+                #calEvents = requests.get("https://"+API_URL+API_EXT+"calendar_events?type=assignment&access_token="+ACCESS_TOKEN+"&context_codes%5B%5D=course_" + str(elem["id"]))
+                #calEvents = requests.get("https://"+API_URL+API_EXT+"calendar_events?type=assignment" + "&context_codes%5B%5D=course_" + str(elem["id"]), {"Authorization": f"bearer {ACCESS_TOKEN}"})
+                #st.write(calEvents.json())
+                #st.write("Assignments Due TODAY:" + today)
+                #assignments = requests.get("https://"+API_URL+API_EXT+"calendar_events?access_token=" + ACCESS_TOKEN + "&type=assignment&context_codes%5B%5D=user_" + str(userJson["id"]) + "&context_codes%5B%5D=course_" + str(elem["id"]) + "&start_date=" + yesterday + "&end_date=" + today + "&per_page=50").json()
+                #for ass in assignments:
+                #    st.write(ass["title"])
 
 classes_page()
