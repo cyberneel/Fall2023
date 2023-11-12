fef9d5afa655749c493e41ee8611e98259312407
cyberneel <neelesh.chevuri25@gmail.com>
2023-11-11 20:53:09 -0600

    Due Today List (Offset Bad needs fix)

--- main_script.py
+++ main_script.py
@@ -4,6 +4,7 @@ import streamlit as st
 import altair as alt
 from streamlit_calendar import calendar
 import requests
+import datetime
 
 #API_URL = "https://unt.instructure.com/api/v1/"
 API_KEY = "9082~1MuaTSggWfo8LFKsjyVFYGdbBV8KvK4RbfTGoiBtU8oEdVpNoPD2ipX2Fp3i4fKf"
@@ -12,6 +13,9 @@ API_EXT = '/api/v1/'
 
 API_URL = ""
 ACCESS_TOKEN = "" 
+
+yesterday = (datetime.datetime.today() + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')
+today = (datetime.datetime.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 
 st.set_page_config(
     page_title="OnTime",
@@ -39,7 +43,10 @@ def classes_page():
             #calEvents = requests.get("https://"+API_URL+API_EXT+"calendar_events?type=assignment&access_token="+ACCESS_TOKEN+"&context_codes%5B%5D=course_" + str(elem["id"]))
             #calEvents = requests.get("https://"+API_URL+API_EXT+"calendar_events?type=assignment" + "&context_codes%5B%5D=course_" + str(elem["id"]), {"Authorization": f"bearer {ACCESS_TOKEN}"})
             #st.write(calEvents.json())
-            st.write(requests.get("https://unt.instructure.com/api/v1/calendar_events?access_token=" + ACCESS_TOKEN + "&type=assignment&context_codes%5B%5D=user_" + str(userJson["id"]) + "&context_codes%5B%5D=course_90487&context_codes%5B%5D=course_90493&context_codes%5B%5D=course_90960&context_codes%5B%5D=course_90968&context_codes%5B%5D=course_95363&context_codes%5B%5D=course_92626&context_codes%5B%5D=course_68254&context_codes%5B%5D=account_440&start_date=2023-10-29T05%3A00%3A00.000Z&end_date=2023-12-03T06%3A00%3A00.000Z&per_page=50").json())
+            st.write("Assignments Due TODAY:" + today)
+            assignments = requests.get("https://unt.instructure.com/api/v1/calendar_events?access_token=" + ACCESS_TOKEN + "&type=assignment&context_codes%5B%5D=user_" + str(userJson["id"]) + "&context_codes%5B%5D=course_" + str(elem["id"]) + "&start_date=" + yesterday + "&end_date=" + today + "&per_page=50").json()
+            for ass in assignments:
+                st.write(ass["title"])
 
 if __name__ == '__main__':
 
