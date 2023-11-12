7ae2a24f45b45248cce9738e8b07088b85ae59f3
Elitelord <73070716+Elitelord@users.noreply.github.com>
2023-11-11 21:51:00 -0600

    Finished option menu

--- main_script.py
+++ main_script.py
@@ -4,13 +4,12 @@ import streamlit as st
 import altair as alt
 from streamlit_calendar import calendar
 import requests
-from streamlit_optionmenu import option_menu
+from streamlit_option_menu import option_menu
 import datetime
 
-#API_URL = "https://unt.instructure.com/api/v1/"
+API_URL = "https://unt.instructure.com/api/v1/"
 API_KEY = "9082~1MuaTSggWfo8LFKsjyVFYGdbBV8KvK4RbfTGoiBtU8oEdVpNoPD2ipX2Fp3i4fKf"
 API_KEYM = "9082~PoqZCFiKGh0YegeAT4EBxzgUbdwuQcn8SIE1EAOTC07btruXEpbWQCNAmY8pdaz0"
-API_EXT = '/api/v1/'
 
 API_URL = ""
 ACCESS_TOKEN = "" 
@@ -26,6 +25,10 @@ st.set_page_config(
 
 def home_page():
     st.title('Welcome to OnTime!')
+    response = requests.get(API_URL+"courses?access_token="+API_KEY)
+    #st.write(response.json()[0]["name"])
+    for elem in response.json():
+        st.write(elem["name"])
 def task_page():
     st.title("Project Dashboard")
     st.divider() 
@@ -52,18 +55,22 @@ def classes_page():
             assignments = requests.get("https://unt.instructure.com/api/v1/calendar_events?access_token=" + ACCESS_TOKEN + "&type=assignment&context_codes%5B%5D=user_" + str(userJson["id"]) + "&context_codes%5B%5D=course_" + str(elem["id"]) + "&start_date=" + yesterday + "&end_date=" + today + "&per_page=50").json()
             for ass in assignments:
                 st.write(ass["title"])
-
 if __name__ == '__main__':
 
     with st.sidebar:
         selected = option_menu(None, ["Home","Calendar", "Settings", "Classes", "Tasks"],
-        icons = ['house',  'date', 'gear'], default_index = 1)
+        icons = ['house',  'calendar', 'gear', "book", "pencil"], default_index = 1,
+        styles={"container": {"padding": "0!important", "background-color": "#120101"},
+        "icon": {"color": "orange", "font-size": "25px"},
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#a6874b"},
+        "nav-link-selected": {"background-color": "green"},
+                }
+                               )
         selected
-    selected_page = st.sidebar.selectbox(
-        'Select Page',
-        ('Home', 'Calendar', 'Classes')
-    )
-
+    #selected_page = st.sidebar.selectbox(
+     #   'Select Page',
+    #    ('Home', 'Calendar', 'Settings')
+    #)
     if "API_URL" not in st.session_state:
         st.session_state["API_URL"] = ""
 
@@ -73,7 +80,13 @@ if __name__ == '__main__':
     API_URL = st.session_state["API_URL"]
     ACCESS_TOKEN = st.session_state["ACCESS_TOKEN"]
 
-    if selected_page == 'Home':
+    if selected == 'Home':
         home_page()
-    elif selected_page == 'Classes':
+    elif selected == 'Tasks':
+        task_page()
+    elif selected == 'Calendar':
+        task_page()
+    elif selected == 'Settings':
+        task_page()
+    elif selected == 'Classes':
         classes_page()
