ddb83b758c2021859f3155855493b8a2ca249f2a
cyberneel <neelesh.chevuri25@gmail.com>
2023-11-11 20:32:41 -0600

    working assignments list

--- main_script.py
+++ main_script.py
@@ -5,9 +5,13 @@ import altair as alt
 from streamlit_calendar import calendar
 import requests
 
-API_URL = "https://unt.instructure.com/api/v1/"
+#API_URL = "https://unt.instructure.com/api/v1/"
 API_KEY = "9082~1MuaTSggWfo8LFKsjyVFYGdbBV8KvK4RbfTGoiBtU8oEdVpNoPD2ipX2Fp3i4fKf"
 API_KEYM = "9082~PoqZCFiKGh0YegeAT4EBxzgUbdwuQcn8SIE1EAOTC07btruXEpbWQCNAmY8pdaz0"
+API_EXT = '/api/v1/'
+
+API_URL = ""
+ACCESS_TOKEN = "" 
 
 st.set_page_config(
     page_title="OnTime",
@@ -17,17 +21,43 @@ st.set_page_config(
 
 def home_page():
     st.title('Welcome to OnTime!')
-    response = requests.get(API_URL+"courses?access_token="+API_KEY)
+
+def classes_page():
+    userJson = requests.get("https://unt.instructure.com/api/v1/users/self?access_token="+ACCESS_TOKEN).json()
+    st.title("Your Classes:")
+    response = requests.get("https://"+API_URL+API_EXT+"courses?access_token="+ACCESS_TOKEN)
     #st.write(response.json()[0]["name"])
     for elem in response.json():
-        st.write(elem["name"])
+        with st.expander(elem["name"]):
+            if("original_name" in elem):
+                st.write("Original Name: " + elem["original_name"])
+            else:
+                st.write("Original Name: " + elem["name"])
+
+            st.write("Course Code: " + elem["course_code"])
+
+            #calEvents = requests.get("https://"+API_URL+API_EXT+"calendar_events?type=assignment&access_token="+ACCESS_TOKEN+"&context_codes%5B%5D=course_" + str(elem["id"]))
+            #calEvents = requests.get("https://"+API_URL+API_EXT+"calendar_events?type=assignment" + "&context_codes%5B%5D=course_" + str(elem["id"]), {"Authorization": f"bearer {ACCESS_TOKEN}"})
+            #st.write(calEvents.json())
+            st.write(requests.get("https://unt.instructure.com/api/v1/calendar_events?access_token=" + ACCESS_TOKEN + "&type=assignment&context_codes%5B%5D=user_" + str(userJson["id"]) + "&context_codes%5B%5D=course_90487&context_codes%5B%5D=course_90493&context_codes%5B%5D=course_90960&context_codes%5B%5D=course_90968&context_codes%5B%5D=course_95363&context_codes%5B%5D=course_92626&context_codes%5B%5D=course_68254&context_codes%5B%5D=account_440&start_date=2023-10-29T05%3A00%3A00.000Z&end_date=2023-12-03T06%3A00%3A00.000Z&per_page=50").json())
 
 if __name__ == '__main__':
 
     selected_page = st.sidebar.selectbox(
         'Select Page',
-        ('Home', 'Calendar', 'Settings')
+        ('Home', 'Calendar', 'Classes')
     )
+
+    if "API_URL" not in st.session_state:
+        st.session_state["API_URL"] = ""
+
+    if "ACCESS_TOKEN" not in st.session_state:
+        st.session_state["ACCESS_TOKEN"] = ""
+
+    API_URL = st.session_state["API_URL"]
+    ACCESS_TOKEN = st.session_state["ACCESS_TOKEN"]
 
     if selected_page == 'Home':
         home_page()
+    elif selected_page == 'Classes':
+        classes_page()
--- pages/settings.py
+++ pages/settings.py
@@ -3,5 +3,5 @@ import streamlit as st
 API_URL = 'unt.instructure.com'
 ACCESS_TOKEN = 'find this in profile settings'
 
-API_URL = st.text_input('Please enter your organization\'s Canvas Instructure URL: ', API_URL)
-ACCESS_TOKEN = st.text_input('Please enter your Access Token: ', ACCESS_TOKEN)
+st.session_state["API_URL"] = st.text_input('Please enter your organization\'s Canvas Instructure URL: ', st.session_state["API_URL"])
+st.session_state["ACCESS_TOKEN"] = st.text_input('Please enter your Access Token: ', st.session_state["ACCESS_TOKEN"])
