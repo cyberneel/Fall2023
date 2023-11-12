db99c18d17cc82660cba23e237165ae96b584875
Elitelord <73070716+Elitelord@users.noreply.github.com>
2023-11-12 10:33:20 -0600

    fixed devsocials

--- pages/devs-socials.py
+++ pages/devs-socials.py
@@ -6,11 +6,11 @@ with col1:
     st.link_button("Neelesh Chevuri", "https://instagram.com/cyber_neel")
     
 with col2:
-    st.link_button("Yugarya Goyal", "https://instagram.com/cyber_neel")
+    st.link_button("Yugarya Goyal", "https://facebook.com/yugarya.goyal")
 
 with col3:
-    st.link_button("Sameer Agarwal", "https://instagram.com/cyber_neel")
+    st.link_button("Sameer Agarwal", "https://instagram.com/sam.eer.agarwal")
 
 with col4:
-    st.link_button("Murtaza Haque", "https://instagram.com/cyber_neel")
+    st.link_button("Murtaza Haque", "https://www.facebook.com/profile.php?id=100093926327175")
 
