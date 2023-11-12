48c002377822cfedc379f77b454ba8d120cc3629
Elitelord <73070716+Elitelord@users.noreply.github.com>
2023-11-12 00:40:21 -0600

    trying to fix settings.py

--- pages/settings.py
+++ pages/settings.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
 API_URL = 'unt.instructure.com'
-ACCESS_TOKEN = 'find this in profile settings'
+API_KEY = "9082~1MuaTSggWfo8LFKsjyVFYGdbBV8KvK4RbfTGoiBtU8oEdVpNoPD2ipX2Fp3i4fKf"
+API_KEYM = "9082~PoqZCFiKGh0YegeAT4EBxzgUbdwuQcn8SIE1EAOTC07btruXEpbWQCNAmY8pdaz0"
 
+ACCESS_TOKEN = 'find this in profile settings'
+API_URL = st.session_state["API_URL"]
 st.session_state["API_URL"] = st.text_input('Please enter your organization\'s Canvas Instructure URL: ', st.session_state["API_URL"])
 st.session_state["ACCESS_TOKEN"] = st.text_input('Please enter your Access Token: ', st.session_state["ACCESS_TOKEN"])
