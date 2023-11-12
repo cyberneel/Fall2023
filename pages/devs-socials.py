75dce79571a893deaa40d053144558a3e07b14a6
cyberneel <neelesh.chevuri25@gmail.com>
2023-11-12 10:06:47 -0600

    Create devs-socials.py

---
+++ pages/devs-socials.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.link_button("Neelesh Chevuri", "https://instagram.com/cyber_neel")
+    
+with col2:
+    st.link_button("Yugarya Goyal", "https://instagram.com/cyber_neel")
+
+with col3:
+    st.link_button("Sameer Agarwal", "https://instagram.com/cyber_neel")
+
+with col4:
+    st.link_button("Murtaza Haque", "https://instagram.com/cyber_neel")
+
