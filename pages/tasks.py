37d71978ed4bf45eb82e4c519e4f0a0ceca6ef75
Elitelord <73070716+Elitelord@users.noreply.github.com>
2023-11-12 01:21:50 -0600

    Added optionmenu for tasks

--- pages/tasks.py
+++ pages/tasks.py
@@ -1,5 +1,39 @@
 import streamlit as st
+import pandas as pd
+from PIL import Image
+import streamlit as st
+import altair as alt
+from streamlit_calendar import calendar
+import requests
+from streamlit_option_menu import option_menu
+import datetime
 from pages.settings import settings_page
 from pages.classes import classes_page
-st.title("Project Dashboard")
-st.divider() 
+
+API_URL = "https://unt.instructure.com"
+API_KEY = "9082~1MuaTSggWfo8LFKsjyVFYGdbBV8KvK4RbfTGoiBtU8oEdVpNoPD2ipX2Fp3i4fKf"
+API_KEYM = "9082~PoqZCFiKGh0YegeAT4EBxzgUbdwuQcn8SIE1EAOTC07btruXEpbWQCNAmY8pdaz0"
+
+
+ACCESS_TOKEN = st.session_state["ACCESS_TOKEN"]
+API_URL = st.session_state["API_URL"]
+
+API_EXT = "/api/v1/"
+
+yesterday = datetime.datetime.combine((datetime.datetime.today() + datetime.timedelta(days=0)), datetime.time.min).strftime('%Y-%m-%d')
+today = datetime.datetime.combine((datetime.datetime.today() + datetime.timedelta(days=0)), datetime.time.max).strftime('%Y-%m-%d')
+
+def task_page():
+    st.title("Task Dashboard")
+    st.divider() 
+    selected2 = option_menu(None, ["Due today", "Next 7 days", "Next 30 Days"],
+    default_index = 0, orientation = "horizontal",
+    styles = {
+        "nav-link": {"--hover-color": "#75340e"}
+    })
+    selected2   
+    st.write("Assignments Due TODAY:" + today)
+    assignments = requests.get("https://"+API_URL+API_EXT+"calendar_events?access_token=" + ACCESS_TOKEN + "&type=assignment&context_codes%5B%5D=user_" + str(userJson["id"]) + "&context_codes%5B%5D=course_" + str(elem["id"]) + "&start_date=" + yesterday + "&end_date=" + today + "&per_page=50").json()
+    for ass in assignments:
+        st.write(ass["title"])
+task_page()
